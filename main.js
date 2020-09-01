5b52d59184ead89441bc75ea7e0735ac13ccf4c7
sanaanymoe <sanaany1992@gmail.com>
2020-09-01 15:48:20 -0700

    add date

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 
-function setDate() {
+// function setDate(){
+//     n = new Date();
+//     year = n.getFullYear();
+//     month = n.getMonth() + 1;
+//     day = n.getDate();
+//     document.getElementById("date").innerHTML = month + "/" + day + "/" + year;;
+// }
+
+function setTime() {
+
+    n = new Date();
+    year = n.getFullYear();
+    month = n.getMonth() + 1;
+    day = n.getDate();
+    document.getElementById("date").innerHTML = month + "/" + day + "/" + year;
+
     const date = new Date();
     const h = date.getHours();
     const m = date.getMinutes();
     const s = date.getSeconds();
     const hourHand = 30*h + m/2;
-    const minuteHand = 6*m;
-    const seconHand = 6*s;
+    const minuteHand = 6*m + 90;
+    const seconHand = 6*s + 90 ;
     
     hour.style.transform = `rotate(${hourHand}deg)`;
     minute.style.transform = `rotate(${minuteHand}deg)`;
     second.style.transform = `rotate(${seconHand}deg)`;
 };
+// setDate()
 
-setInterval(setDate, 1000);
+setInterval(setTime, 1000);
 
